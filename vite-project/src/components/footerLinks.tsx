2842e662e359e31d912b79649ab4412e967a006c
Abdelraman Ahmed <AbdelramanAhmed3T3@gmail.com>
2026-03-19 00:08:49 +0200

    make a nav bar and footer

---
+++ vite-project/src/components/footerLinks.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import { Link } from 'react-router-dom'
+
+
+type link = {
+    name:string,
+    path : string
+}
+
+type LinksType = {
+    links:link[]
+}
+
+const FooterLinks = ({links}:LinksType) => {
+  
+  console.log(links);
+  
+    return (
+
+    <ul className='flex flex-col gap-2'>
+    {links.map((i) =>{
+        return(
+            <li className="text-gray-400 font-bold w-fit hover:ml-3 hover:text-primary  duration-700 relative after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[1.5px] after:bg-primary hover:after:w-full after:transition-all"> <Link to={i.path}>{i.name}</Link></li>
+        )
+    })}
+    </ul>
+  )
+}
+
+export default FooterLinks
